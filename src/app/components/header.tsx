6c0f44007871c39741661ea788febe8da67e88e9
SteffenHF <120659330+SteffenHF@users.noreply.github.com>
2024-09-30 14:41:18 +0000

    use nextui for components

--- src/app/components/header.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Header() {
-    return (
-        <header className="flex flex-row place-content-between p-1">
-            <h1 className="text-2xl font-semibold">HFS Sales</h1>
-        </header>
-    );
-}
