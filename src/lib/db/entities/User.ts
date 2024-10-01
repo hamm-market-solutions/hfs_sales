feeb25b0b942323493f2af9f856c293da195d113
SteffenHF <120659330+SteffenHF@users.noreply.github.com>
2024-10-01 13:44:58 +0000

    fix devcontainer setup

--- src/lib/db/entities/User.ts
+++ src/lib/db/entities/User.ts
@@ -3,17 +3,17 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
-  id: number;
+  id!: number;
 
   @Column({ length: 50 })
-  fname: string;
+  fname?: string;
 
   @Column({ length: 50 })
-  name: string;
+  name!: string;
 
   @Column({ length: 70 })
-  password: string;
+  password!: string;
 
   @Column({ length: 100 })
-  email: string;
+  email!: string;
 }
